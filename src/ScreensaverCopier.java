bc39a696617f73023f49b1481a2ffa1643fc5be7
Евгений <30576507+Sambalinski@users.noreply.github.com>
2019-07-12 20:48:58 +0300

    Some refactoring
    add checkDirectory method

--- src/ScreensaverCopier.java
+++ src/ScreensaverCopier.java
@@ -5,35 +5,45 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class ScreensaverCopier {
-    final static String src = "c:/Users/Sambalinski/AppData/Local/Packages/Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy/LocalState/Assets/";
-    final static String dst = "d:/Pictures/Assets Img/";
+    private final static String src = "c:/Users/1072938/AppData/Local/Packages/Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy/LocalState/Assets/";
+    private final static String dst = "c:/Users/1072938/desktop/Pictures/Assets Img/";
+
+
     public static void main(String[] args) throws IOException {
         Path source = Paths.get(src);
         Path destination = Paths.get(dst);
-
-        if (Files.notExists(destination)) Files.createDirectory(destination);
+        
+        checkDirectory(destination);
 
         Files.walk(source)
                 .filter(s -> Files.isRegularFile(s))
-                .filter(s -> returnBig(s))
+                .filter(ScreensaverCopier::returnBig)
                 .forEach(s -> {
-                    Path randomName = Paths.get(destination + File.separator + String.valueOf((int) (Math.random() * 1000000)) + ".jpg");
+                    Path randomName = Paths.get(destination + File.separator + (int) (Math.random() * 1000000) + ".jpg");
                     copy(s, randomName);
                 });
     }
-
-    public static void copy(Path sourse, Path dst) {
+    
+    @SuppressWarnings("ResultOfMethodCallIgnored")
+    private static void checkDirectory(Path destination) {
+        if (Files.notExists(destination))
+            destination.toFile().mkdirs();
+    }
+    
+    private static void copy(Path source, Path dst) {
         try {
-            Files.copy(sourse, dst);
+            Files.copy(source, dst);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public static boolean returnBig (Path path) {
-        try{
-        return Files.size(path) > 150000;}
-        catch (IOException e) {e.printStackTrace();}
+    private static boolean returnBig(Path path) {
+        try {
+            return Files.size(path) > 150000;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return true;
     }
 }
